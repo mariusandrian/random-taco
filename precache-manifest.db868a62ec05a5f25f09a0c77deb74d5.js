7181f2399b3c7b93a7ef95ad33ad36dd43ff6e83
Marius Andrian <marius.atj@gmail.com>
2020-07-18 12:51:06 +0800

    Updates

---
+++ precache-manifest.db868a62ec05a5f25f09a0c77deb74d5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3dd5a4fdec6148d64cadedfb0e3955ba",
+    "url": "/random-taco/index.html"
+  },
+  {
+    "revision": "8b4449bfeb83c94b7d2d",
+    "url": "/random-taco/static/css/main.0487316e.chunk.css"
+  },
+  {
+    "revision": "abdecab4d3a5771ddecf",
+    "url": "/random-taco/static/js/2.ec6ee4b5.chunk.js"
+  },
+  {
+    "revision": "40046c021c0025c71b5ec67323b92a85",
+    "url": "/random-taco/static/js/2.ec6ee4b5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8b4449bfeb83c94b7d2d",
+    "url": "/random-taco/static/js/main.bce6ab1c.chunk.js"
+  },
+  {
+    "revision": "23ece864cc3d9b16ceb9",
+    "url": "/random-taco/static/js/runtime-main.afecf583.js"
+  }
+]);
